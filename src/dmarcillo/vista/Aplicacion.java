e34e5af909b6dcaf13e264e1287744f1b99b00a6
LAB1 <LAB1@LAB1PC12018>
2019-02-26 08:29:45 -0500

    Examen_Programacion

---
+++ src/dmarcillo/vista/Aplicacion.java
@@ -0,0 +1,15 @@
+package dmarcillo.vista;
+
+
+import dmarcillo.modelo.VentanaPuntos;
+
+/**
+ *
+ * @author 
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        new VentanaPuntos();
+    }
+    
+}
